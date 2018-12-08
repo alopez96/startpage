7916ae63ee171eb28fda764044fdf65bff4dd2cd
Arturo <alopez96@ucsc.edu>
2018-12-08 13:29:31 -0800

    set links for each button

--- src/components/ActionButtons.js
+++ src/components/ActionButtons.js
@@ -1,29 +1,41 @@
 import React, {Component} from 'react'
-import logo from './logo.svg'
-import { Button, Icon } from 'semantic-ui-react'
+import { Icon } from 'semantic-ui-react'
 import './ActionButtons.css';
-import styled from 'styled-components';
 
 class ActionButtons extends Component {
     render(){
         return(
             <div>  
-                <img src={logo} className="App-logo" alt="logo" />
                 <div>
                 <Icon.Group size='big' className='padded'>
+                <a className='white-button' 
+                    href='https://github.com/alopez96'>
                     <Icon color='white' name='github' />
+                </a>
                 </Icon.Group>
                 <Icon.Group size='big' className='padded'>
+                <a className='white-button'
+                    href='https://mail.google.com/mail/u/0/#inbox'>
                     <Icon name='mail' />
+                    </a>
                 </Icon.Group> 
                 <Icon.Group size='big' className='padded'>
+                <a className='white-button'
+                    href='https://www.reddit.com/'>
                     <Icon name='reddit' />
+                </a>
                 </Icon.Group> 
                 <Icon.Group size='big' className='padded'>
+                <a className='white-button'
+                href='https://twitter.com/'>
                     <Icon name='twitter' />
+                </a>
                 </Icon.Group> 
                 <Icon.Group size='big' className='padded'>
+                <a className='white-button'
+                    href='https://www.linkedin.com/feed/'>
                     <Icon name='linkedin' />
+                </a>
                 </Icon.Group> 
                 </div> 
             </div>
