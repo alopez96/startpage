9d5a21db0b7b5602241eddd625d3687d69d6f2a7
Arturo <alopez96@ucsc.edu>
2018-12-08 19:44:00 -0800

    set date and time

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,11 +4,31 @@ import './Header.css'
 
 
 class Header extends Component {
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+          time: null,
+        };
+      }
+
+    componentDidMount() {
+        //display time and update every second
+          setInterval(() => this.setState({
+            time: new Date().toLocaleString()
+            }), 1000)   
+    }
+
     render(){
         return(
             <div>  
                 <div className='header'>
                 <h1>Hello Arturo</h1>
+                {
+                    this.state.time
+                    ?<h2>Today is {this.state.time}</h2>
+                    :null
+                }
                 </div> 
             </div>
         )
